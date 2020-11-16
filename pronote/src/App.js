e2c82305e795728471eb080cd0a397cebff8cfb7
chrysleremmanuel123 <chrysler.emmanuel@gmail.com>
2020-11-17 01:25:58 +0530

    done

---
+++ pronote/src/App.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+import React from 'react';
+import Editor from './components/Editor';
+import './App.css';
+import './Editor.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Editor />
+    </div>
+  );
+}
+
+export default App;
